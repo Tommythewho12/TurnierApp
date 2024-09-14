19cff45016ac8a3de9c6fcc3175c2a6ca6c58726
Tommy <tommythewho12@googlemail.com>
2024-09-14 15:28:43 +0200

    display teams statistics in group overview (phase-view.component.js)

--- frontend/react-crud/src/components/phase-view.component.js
+++ frontend/react-crud/src/components/phase-view.component.js
@@ -4,6 +4,13 @@ import MatchDataService from "../services/match.service.js";
 import { Link } from "react-router-dom";
 import { withRouter } from '../common/with-router';
 
+class Team {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
 class PhaseView extends Component {
     constructor(props) {
         super(props);
@@ -15,102 +22,87 @@ class PhaseView extends Component {
 
     componentDidMount() {
         this.getGroups();
-        console.log("finally", this.state);
     }
 
     getGroups() {
         GroupDataService.getByPhaseId(this.props.router.params.id)
             .then(response => {
-                this.setState({
-                    groups: response.data
-                });
-                this.getMatchs();
+                this.getMatchs(response.data);
             })
             .catch(e => {
                 console.log(e);
             });
     }
 
-    getMatchs() {
-        this.state.groups.forEach((group, index) => {
-            MatchDataService.getByGroupId(group._id)
+    async getMatchs(groups) {
+        const promises = [];
+        groups.forEach((group, index) => {
+            const promise = MatchDataService.getByGroupId(group._id)
                 .then(response => {
-                    this.handleshit(index, response.data);
-                    this.calculateStuff();
+                    group.matchs = response.data;
                 })
                 .catch(e => {
                     console.log(e);
-                })
-        });
-    }
-
-    handleshit(index, matchs) {
-        const nextGroups = this.state.groups.map((group, i) => {
-            if (i === index) {
-                group.matchs = matchs;
-                this.getTeamInfo(group, matchs);
-                return group;
-            } else {
-                return group;
-            }
-        });
-        console.log("nextGroups", nextGroups);
-        this.setState(nextGroups);
-    }
-
-    getTeamInfo(group, matchs) {
-        // find all teams for each group to display
-        const teams = new Set();
-        console.log("new set");
-        const x = matchs.map((match) => {
-            teams.add(match.homeTeam); // make sure only adding homeTeam fills the lists properly
-            // TODO count points for teams as well
-            console.log("teams", teams);
-        })
-        group.teams = Array.from(teams);
-        return;
-    }
-
-    calculateScores() {
-        this.groups.forEach(group => {
-            group.matchs.forEach(match => {
-                let homeScore = 0, guestScore = 0;
-                match.sets.forEach(set => {
-                    homeScore += set.homeScore;
-                    guestScore += set.guestScore;
                 });
-            })
-        })
+            promises.push(promise);
+        });
+        await Promise.all(promises);
+        this.calculateScoreForEachTeam(groups);
     }
 
-    calculateStuff() {
-        let teams = new Map();
-        
-        this.state.groups.forEach(group => {
+    calculateScoreForEachTeam(groups) {
+        // this loop creates a map 'teams' and calculates the stats for each
+        groups.forEach(group => {
+            let teams = {};
             group.matchs.forEach(match => {
-                if (!teams.has(match.homeTeam)) {
-                    teams.set(match.homeTeam, {score: 0});
+                const homeId = match.homeTeam._id, guestId = match.guestTeam._id;
+                if (!teams[homeId]) {
+                    teams[homeId] = {id: homeId, name: match.homeTeam.name, score: 0, matches: 0, wins: 0, losses: 0, draws: 0, pointsScored: 0, pointsSuffered: 0};
                 }
-                if (!teams.has(match.guestTeam)) {
-                    teams.set(match.guestTeam, {score: 0});
+                if (!teams[guestId]) {
+                    teams[guestId] = {id: guestId, name: match.guestTeam.name, score: 0, matches: 0, wins: 0, losses: 0, draws: 0, pointsScored: 0, pointsSuffered: 0};
                 }
+
+                teams[homeId].matches++;
+                teams[guestId].matches++;
+
+                let homeSets = 0, guestSets = 0;
                 match.sets.forEach(set => {
-                    teams.get(match.homeTeam).score += Number(set.scoreHome);
-                    teams.get(match.guestTeam).score += Number(set.scoreGuest);
+                    const scoreHome = Number(set.scoreHome), scoreGuest = Number(set.scoreGuest);
+
+                    teams[homeId].score += scoreHome;
+                    teams[guestId].score += scoreGuest;
+
+                    if (scoreHome > scoreGuest) homeSets++;
+                    else if (scoreHome < scoreGuest) guestSets++;
+
+                    teams[homeId].pointsScored += scoreHome;
+                    teams[homeId].pointsSuffered += scoreGuest;
+                    teams[guestId].pointsScored += scoreGuest;
+                    teams[guestId].pointsSuffered += scoreHome;
                 });
-            })
+                if (homeSets > guestSets) {
+                    teams[homeId].wins++;
+                    teams[guestId].losses++;
+                } else if (homeSets < guestSets) {
+                    teams[guestId].wins++;
+                    teams[homeId].losses++;
+                } else {
+                    teams[homeId].draws++;
+                    teams[guestId].draws++;
+                }
+            });
+            group.teams = teams;
         });
-        console.log("calculateStuff.teams", teams);
-
-
+        this.setState({groups: groups});
     }
 
     render() {
-        const { currentTutorial } = this.state;
+        const { groups } = this.state;
 
         return (
             <div>
-                {this.state.groups && this.state.groups.map(group => (
+                {groups && groups.map(group => (
                     <div className="group-container">
                         <div className="group-header">
                         </div>
@@ -121,22 +113,20 @@ class PhaseView extends Component {
                                         <th></th>
                                         <th>Spiele</th>
                                         <th>Siege</th>
-                                        <th>Remis</th>
                                         <th>Niederlagen</th>
                                         <th>Punkte</th>
                                         <th>Punktdifferenz</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {group.teams && group.teams.map(team => (
+                                    {group.teams && Object.values(group.teams).map(team => (
                                         <tr>
-                                            <td>{team ? (team.name) : "penis"}</td>
-                                            <td>Spiele</td>
-                                            <td>Siege</td>
-                                            <td>Remis</td>
-                                            <td>Niederlagen</td>
-                                            <td>Punkte</td>
-                                            <td>Punktdifferenz</td>
+                                            <td>{team.name}</td>
+                                            <td>{team.matches}</td>
+                                            <td>{team.wins}</td>
+                                            <td>{team.losses}</td>
+                                            <td>{team.score}</td>
+                                            <td>{team.pointsScored} : {team.pointsSuffered}</td>
                                         </tr>
                                     ))}
                                 </tbody>
