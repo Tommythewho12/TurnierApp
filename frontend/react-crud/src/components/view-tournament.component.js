611f4fa6872033fabe4d2bd3a765a30d12eeb847
Tommy <tommythewho12@googlemail.com>
2024-10-21 22:06:09 +0200

    add information to phases > 0

--- frontend/react-crud/src/components/view-tournament.component.js
+++ frontend/react-crud/src/components/view-tournament.component.js
@@ -32,18 +32,20 @@ class ViewTournament extends Component {
     enrichTeamStats(tournament) {
         tournament.phases.forEach((phase, phaseIndex, phases) => {
             phase.groups.forEach((group, groupIndex, groups) => {
-                group.teams = group.teams.map(team => (
-                    {
-                        _id: team,
-                        name: tournament.teams.find(t => t._id === team).name,
-                        score: 0,
-                        matchs: 0,
-                        wins: 0,
-                        losss: 0,
-                        draws: 0,
-                        pointsScored: 0,
-                        pointsSuffered: 0
-                    }));
+                if (group.teams != undefined && group.teams.length > 0) {
+                    group.teams = group.teams.map(team => (
+                        {
+                            _id: team,
+                            name: tournament.teams.find(t => t._id === team).name,
+                            score: 0,
+                            matchs: 0,
+                            wins: 0,
+                            losss: 0,
+                            draws: 0,
+                            pointsScored: 0,
+                            pointsSuffered: 0
+                        }));
+                }
                 group.matchs.forEach((match, matchIndex, matchs) => {
                     if (match.concluded) { // TODO this will not allow to see live results / allow only finished results
                         const homeTeam = group.teams.find(t => t._id === match.homeTeam);
@@ -83,7 +85,7 @@ class ViewTournament extends Component {
             });
         });
         
-        
+        console.log("tournament: ", tournament);
         this.setState({tournament: tournament});
     }
 
@@ -94,6 +96,18 @@ class ViewTournament extends Component {
             }
         }
         return "n/a";
+    }
+
+    getDescriptionFromTeamReference(teamRef) {
+        let result = "";
+        switch(teamRef.rank) {
+            case 0: result += "1st"; break;
+            case 1: result += "2nd"; break;
+            case 2: result += "3rd"; break;
+            default: result += (teamRef.rank + 1) + "th";
+        }
+        result += " place of group " + (teamRef.group + 1);
+        return result;
     }
 
     render() {
@@ -136,6 +150,16 @@ class ViewTournament extends Component {
                                             <td>{team.losss}</td>
                                             <td>{team.score}</td>
                                             <td>{team.pointsScored} : {team.pointsSuffered}</td>
+                                        </tr>
+                                    ))}
+                                    {group.teamReferences != null && group.teamReferences.map(teamRef => (
+                                        <tr>
+                                            <td>{this.getDescriptionFromTeamReference(teamRef)}</td>
+                                            <td>0</td>
+                                            <td>0</td>
+                                            <td>0</td>
+                                            <td>0</td>
+                                            <td>0 : 0</td>
                                         </tr>
                                     ))}
                                 </tbody>
