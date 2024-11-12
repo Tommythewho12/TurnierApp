06f5c6c771cbf2c99eaed9dda5d93793ea32ce5f
Tommy <tommythewho12@googlemail.com>
2024-11-12 19:03:35 +0100

    #25 configure site for external access

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "*"
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
